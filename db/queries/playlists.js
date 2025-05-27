1f613948302be9269e9c97aad08b2e24868c1b77
Jose Galeana <kevingaleana@yahoo.com>
2025-05-26 22:51:20 -0500

    Pro all done

--- db/queries/playlists.js
+++ db/queries/playlists.js
@@ -34,3 +34,15 @@ export async function getPlaylistById(id) {
   } = await db.query(sql, [id]);
   return playlist;
 }
+
+export async function getPlaylistsByTrackId(id) {
+  const sql = `
+  SELECT playlists.*
+  FROM
+    playlists
+    JOIN playlists_tracks ON playlists.id = playlists_tracks.playlist_id
+  WHERE playlists_tracks.track_id = $1
+  `;
+  const { rows: playlists } = await db.query(sql, [id]);
+  return playlists;
+}
